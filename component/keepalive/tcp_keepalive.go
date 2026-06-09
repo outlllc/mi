080708e1a6c3eccb5f409edeacabff3461b9973b
outlllc <outlocklike@outlook.com>
2026-06-09 19:35:45 +0800

    u

--- component/keepalive/tcp_keepalive.go
+++ component/keepalive/tcp_keepalive.go
@@ -35,7 +35,7 @@ func KeepAliveInterval() time.Duration {
 
 func SetDisableKeepAlive(disable bool) {
 	if runtime.GOOS == "android" {
-		setDisableKeepAlive(true)
+		setDisableKeepAlive(disable)
 	} else {
 		setDisableKeepAlive(disable)
 	}
